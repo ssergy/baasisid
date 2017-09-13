8fc058229bb4f46cc17674b8ccaf1a1c6a69a9ee
ssergy <sergysh@gmail.com>
2017-09-13 23:34:03 +0300

    documents

---
+++ login.sh
@@ -0,0 +1,5 @@
+curl \
+    -H "Content-Type: application/json" \
+    -X POST -d '{"key": "BAASIS_API_KEY", "login": "7-9185003911", "password":"demo"}' \
+    https://api.baasisid.com/auth/baasid-login
+
---
+++ send-sms.sh
@@ -0,0 +1,4 @@
+curl \
+    -H "Content-Type: application/json" \
+    -X POST -d '{"user_hash": "c3732a00d5e297ba5a83504dfe1f2cdb310d0f80", "check_id": 890}' \
+    https://api.baasisid.com/auth/send-sms
